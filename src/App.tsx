3c536096d15918bc785c4064c88dd434a0231094
Shanto Paul <shantopaul470@gmail.com>
2026-09-13 22:18:31 +0600

    changing json path to fix live link error

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Footer from "./Components/Footer"
 
 
 const technologiesFetch = async (): Promise<Itechnology[]> => {
-  const res = await fetch("../public/data.json")
+  const res = await fetch("/data.json")
   const data = await res.json()
   return data
 }
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/B14-ASSIGNMENT-5/',
+  plugins: [react(), tailwindcss()]
+  // base: '/B14-ASSIGNMENT-5/',
 })
